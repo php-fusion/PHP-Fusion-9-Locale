266ed29138615d7718a13abfdf3743c1a5b6e350
karrak1 <g.sebestyen@fusionjatek.hu>
2017-06-25 10:05:42 +0200

    locale git

--- Hungarian/infusions/forum_mods_online_panel/locale/Hungarian.php
+++ Hungarian/infusions/forum_mods_online_panel/locale/Hungarian.php
@@ -3,4 +3,4 @@
 $locale['fmp_0100'] = "Adminok Online";
 $locale['fmp_0101'] = "A vezetőség";
 $locale['fmp_0102'] = "Moderátorok Online";
-$locale['fmp_0103'] = "Kérjük telepítse fórum infusiont";
+$locale['fmp_0103'] = "Kérjük telepítse a fórum infusiont";
