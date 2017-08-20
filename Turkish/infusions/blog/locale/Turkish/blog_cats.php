e1cfc9fa66ef1fb32af068fe6155e6a3e903f866
Türk Android <alper.peri@gmail.com>
2017-08-20 23:35:46 +0300

    Update blog_cats.php

--- Turkish/infusions/blog/locale/Turkish/blog_cats.php
+++ Turkish/infusions/blog/locale/Turkish/blog_cats.php
@@ -1,9 +1,12 @@
 <?php
-$locale['400'] = "Blog Categories";
-$locale['401'] = "Category:";
-$locale['402'] = "Number of Items:";
-$locale['403'] = "None-Categorised Blogs";
-$locale['404'] = "There are no Blog items in this category.";
-$locale['405'] = "More";
-$locale['406'] = "Show All Categories";
-$locale['407'] = "No Blog Categories defined";
+/**
+ * @Translated by MAxwELL_TR Web Site (https://alperperi.com)
+ */
+$locale['400'] = "Blog Kategorileri";
+$locale['401'] = "Kategori:";
+$locale['402'] = "Öğe Sayısı:";
+$locale['403'] = "Kategorisiz Bloglar";
+$locale['404'] = "Bu kategoride hiç blog gönderisi yok.";
+$locale['405'] = "Daha fazla";
+$locale['406'] = "Tüm Kategorileri Göster";
+$locale['407'] = "Tanımlanmış Blog Kategorisi yok";
