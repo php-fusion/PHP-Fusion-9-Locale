ee63fa597d75d312ffd60835abb8e2e4702e89ff
karrak1 <g.sebestyen@fusionjatek.hu>
2017-12-02 17:12:56 +0100

    bbcodes git

--- Hungarian/locale/Hungarian/admin/bbcodes.php
+++ Hungarian/locale/Hungarian/admin/bbcodes.php
@@ -1,24 +1,30 @@
 <?php
-$locale['400'] = "BB kódok szerkesztése";
-$locale['401'] = "BB kódok tesztelése";
+$locale['BBCA_400'] = "BB kódok";
+$locale['BBCA_400a'] = "BB kódok szerkesztése";
+$locale['BBCA_401'] = "BB kódok tesztelése";
 // Enabled BB Codes
-$locale['402'] = "Aktív BB kódok";
-$locale['403'] = "Név";
-$locale['404'] = "Kép";
-$locale['405'] = "Leírás";
-$locale['406'] = "Használat";
-$locale['407'] = "Sorrend";
-$locale['408'] = "Le";
-$locale['409'] = "Fel";
-$locale['410'] = "Kikapcsolás";
-$locale['411'] = "Nincs aktív BB kód";
+$locale['BBCA_402'] = "Aktív BB kódok";
+$locale['BBCA_403'] = "Név";
+$locale['BBCA_404'] = "Kép";
+$locale['BBCA_405'] = "Leírás";
+$locale['BBCA_406'] = "Használat";
+$locale['BBCA_407'] = "Sorrend";
+$locale['BBCA_408'] = "Le";
+$locale['BBCA_409'] = "Fel";
+$locale['BBCA_410'] = "Kikapcsolás";
+$locale['BBCA_411'] = "Nincs aktív BB kód";
 // Disabled BB Codes
-$locale['413'] = "Inaktív BB kódok";
-$locale['414'] = "Bekapcsolás";
-$locale['415'] = "Sortörés beszúrása";
-$locale['416'] = "Nincs inaktív BB kód";
+$locale['BBCA_413'] = "Inaktív BB kódok";
+$locale['BBCA_414'] = "Bekapcsolás";
+//$locale['415'] = "Sortörés beszúrása";
+$locale['BBCA_416'] = "Nincs inaktív BB kód";
 // Test BB Codes
-$locale['417'] = "BB kód teszt eredménye";
-$locale['418'] = "Hangulatjelek kikapcsolása";
-$locale['418a'] = "Teszt üzenet";
-$locale['418b'] = "Kérem adjon meg egy üzenetet, a BB kód tesztjéhez.";
+$locale['BBCA_417'] = "BB kód teszt eredménye";
+$locale['BBCA_418'] = "Hangulatjelek kikapcsolása";
+$locale['BBCA_418a'] = "Teszt üzenet";
+$locale['BBCA_418b'] = "Kérem adjon meg egy üzenetet, a BB kód tesztjéhez.";
+
+$locale['BBCA_430'] = "BB kód felfelé mozgatva.";
+$locale['BBCA_431'] = "BB kód lefelé mozgatva.";
+$locale['BBCA_432'] = "BB kód Bekapcsolva.";
+$locale['BBCA_433'] = "BB kód Kikapcsolva.";
