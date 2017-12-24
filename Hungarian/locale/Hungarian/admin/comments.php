cd787ebf143ab1a93812e911e781b9b4ceb20886
karrak1 <g.sebestyen@fusionjatek.hu>
2017-12-24 17:10:23 +0100

    comment gomb

--- Hungarian/locale/Hungarian/admin/comments.php
+++ Hungarian/locale/Hungarian/admin/comments.php
@@ -10,7 +10,9 @@ $locale['420'] = "Hangulatjelek kikapcsolása ebben a hozzászólásban";
 $locale['421'] = "Hozzászólás mentése";
 // Comments
 $locale['430'] = "Szerkesztés";
-$locale['431'] = "Törlés";
+$locale['431'] = "Törlés és Ban";
 $locale['432'] = "Felhasználó IP:";
 $locale['433'] = "Biztosan törölni szeretnéd ezt a hozzászólást?";
 $locale['434'] = "Erre még nem érkezett hozzászólás.";
+$locale['435'] = "Biztosan Feketelistára teszed ezt a Felhasználót?";
+$locale['436'] = "Hozzászólásai miatt Feketelistára.";
