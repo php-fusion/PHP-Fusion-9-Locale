d528091f02c958124fc6b228a106905bee4467c6
RobiNN <RobiNN1@users.noreply.github.com>
2018-02-03 19:33:21 +0100

    Update Czech locale

--- Czech/locale/Czech/contact.php
+++ Czech/locale/Czech/contact.php
@@ -16,26 +16,26 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 // Contact Form
-$locale['400'] = "Kontakt";
-$locale['401'] = "Existuje několik způsobů, jak nás můžete kontaktovat. Můžete odeslat e-mail přímo na adresu
+$locale['CT_400'] = "Kontakt";
+$locale['CT_401'] = "Existuje několik způsobů, jak nás můžete kontaktovat. Můžete odeslat e-mail přímo na adresu
 [SITE_EMAIL]. Pokud jste členem, můžete poslat [PM_LINK].
 Případně můžete vyplnit formulář na této stránce, který odešle vaši zprávu na náš e-mail.";
-$locale['402'] = "Jméno:";
-$locale['403'] = "E-mailová adresa:";
-$locale['404'] = "Předmět:";
-$locale['405'] = "Zpráva:";
-$locale['406'] = "Odeslat";
-$locale['407'] = "Validační kód:";
-$locale['408'] = "Zadejte validační kód:";
+$locale['CT_402'] = "Jméno:";
+$locale['CT_403'] = "E-mailová adresa:";
+$locale['CT_404'] = "Předmět:";
+$locale['CT_405'] = "Zpráva:";
+$locale['CT_406'] = "Odeslat";
+$locale['CT_407'] = "Validační kód:";
+$locale['CT_408'] = "Zadejte validační kód:";
 // Contact Errors
-$locale['420'] = "Nevyplnili jste jméno";
-$locale['421'] = "Nevyplnili jste e-mailovou adresu";
-$locale['422'] = "Nevyplnili jste předmět";
-$locale['423'] = "Nenapsali jste žádnou zprávu";
-$locale['424'] = "Musíte vložit správný validační kód";
-$locale['425'] = "Vnitřní chyba: Zpráva nebyla odeslána.";
+$locale['CT_420'] = "Nevyplnili jste jméno";
+$locale['CT_421'] = "Nevyplnili jste e-mailovou adresu";
+$locale['CT_422'] = "Nevyplnili jste předmět";
+$locale['CT_423'] = "Nenapsali jste žádnou zprávu";
+$locale['CT_424'] = "Musíte vložit správný validační kód";
+$locale['CT_425'] = "Vnitřní chyba: Zpráva nebyla odeslána.";
 // Message Sent
-$locale['440'] = "Vaše zpráva byla odeslána ";
-$locale['441'] = "Ďekujeme";
-$locale['442'] = "Vaše zpráva nemohla být odeslána a to z těchto důvodů:";
-$locale['443'] = "Zkuste to ještě jednou.";
+$locale['CT_440'] = "Vaše zpráva byla odeslána ";
+$locale['CT_441'] = "Ďekujeme";
+$locale['CT_442'] = "Vaše zpráva nemohla být odeslána a to z těchto důvodů:";
+$locale['CT_443'] = "Zkuste to ještě jednou.";
