551c33802712dc768f511931ba769e14b2f25dae
RobiNN <RobiNN1@users.noreply.github.com>
2018-02-03 19:33:40 +0100

    Update Slovak locale

--- Slovak/locale/Slovak/contact.php
+++ Slovak/locale/Slovak/contact.php
@@ -16,26 +16,26 @@
 | written permission from the original author(s).
 +--------------------------------------------------------*/
 // Contact Form
-$locale['400'] = "Kontakt";
-$locale['401'] = "Je viac spôsobov ako nás môžete kontaktovať. E-mailom na
+$locale['CT_400'] = "Kontakt";
+$locale['CT_401'] = "Je viac spôsobov ako nás môžete kontaktovať. E-mailom na
 [SITE_EMAIL]. Alebo ak ste zaregistrovaný, môžete poslať [PM_LINK].
 Prípadne môžete vyplniť formulár na tejto stránke, ktorý odošle vašu správu na e-mail.";
-$locale['402'] = "Meno:";
-$locale['403'] = "E-mail:";
-$locale['404'] = "Predmet:";
-$locale['405'] = "Správa:";
-$locale['406'] = "Odoslať";
-$locale['407'] = "Validačný kód:";
-$locale['408'] = "Zadajte validačný kód:";
+$locale['CT_402'] = "Meno:";
+$locale['CT_403'] = "E-mail:";
+$locale['CT_404'] = "Predmet:";
+$locale['CT_405'] = "Správa:";
+$locale['CT_406'] = "Odoslať";
+$locale['CT_407'] = "Validačný kód:";
+$locale['CT_408'] = "Zadajte validačný kód:";
 // Contact Errors
-$locale['420'] = "Nevyplnili ste meno";
-$locale['421'] = "Nevyplnili ste e-mailovú adresu";
-$locale['422'] = "Nevyplnili ste predmet";
-$locale['423'] = "Nenapísali ste žiadnu správu";
-$locale['424'] = "Musíte vložiť správny validačný kód";
-$locale['425'] = "Interná chyba: Správa nebola odoslaná.";
+$locale['CT_420'] = "Nevyplnili ste meno";
+$locale['CT_421'] = "Nevyplnili ste e-mailovú adresu";
+$locale['CT_422'] = "Nevyplnili ste predmet";
+$locale['CT_423'] = "Nenapísali ste žiadnu správu";
+$locale['CT_424'] = "Musíte vložiť správny validačný kód";
+$locale['CT_425'] = "Interná chyba: Správa nebola odoslaná.";
 // Message Sent
-$locale['440'] = "Vaša správa bola odoslaná";
-$locale['441'] = "Ďakujem";
-$locale['442'] = "Vaša správa nebola odoslaná, pretože:";
-$locale['443'] = "Skúste poslať ešte raz.";
+$locale['CT_440'] = "Vaša správa bola odoslaná";
+$locale['CT_441'] = "Ďakujem";
+$locale['CT_442'] = "Vaša správa nebola odoslaná, pretože:";
+$locale['CT_443'] = "Skúste poslať ešte raz.";
