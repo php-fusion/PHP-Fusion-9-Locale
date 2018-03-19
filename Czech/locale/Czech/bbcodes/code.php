e051d5932f3fb69e55c6e446827f9c692f9aebc2
RobiNN <RobiNN1@users.noreply.github.com>
2018-03-19 21:09:07 +0100

    Update locale

--- Czech/locale/Czech/bbcodes/code.php
+++ Czech/locale/Czech/bbcodes/code.php
@@ -18,4 +18,4 @@
 $locale['bb_code_description'] = "Zobrazuje text jako kód";
 $locale['bb_code_usage'] = "Nějaký kód";
 $locale['bb_code_code'] = "Kód";
-$locale['bb_code_save'] = "Stáhnout zdroj";
+$locale['bb_code_save'] = "Stáhnout zdrojový kód";
