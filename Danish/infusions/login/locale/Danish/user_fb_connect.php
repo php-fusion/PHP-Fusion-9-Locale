8d06a0b55775d5ae1d759a9b216573a76c52dc21
Helmuth Mikkelsen <helmuthm@gmail.com>
2018-03-30 08:09:14 +0200

    user_fb_connect.php updated

--- Danish/infusions/login/locale/Danish/user_fb_connect.php
+++ Danish/infusions/login/locale/Danish/user_fb_connect.php
@@ -17,10 +17,11 @@
 +--------------------------------------------------------*/
 $locale['uf_fb_connect'] = "Facebook log ind";
 $locale['uf_fb_connect_desc'] = "Facebook log ind connector";
+$locale['uf_fb_connect_verify'] = "Facebook log ind bekræftelse";
 
 // Driver configuration form
 $locale['uf_fb_connect_200'] = "Facebook log ind med Facebook SDK gør det muligt for folk at logge ind med deres Facebook legitimationsoplysninger.";
-$locale['uf_fb_connect_201'] = "Facebook plugin indstillinger";
+$locale['uf_fb_connect_201'] = "Facebook log ind indstillinger";
 $locale['uf_fb_connect_202'] = "Facebook App ID";
 $locale['uf_fb_connect_203'] = "Facebook App Secret"
 $locale['uf_fb_connect_204'] = "Indtast dit Facebook Developer App ID";
@@ -44,14 +45,30 @@ $locale['uf_fb_connect_221'] = "Log ind med ...";
 $locale['uf_fb_connect_222'] = "Aktiver log ud knap";
 $locale['uf_fb_connect_223'] = "Gem Facebook indstillinger";
 
+// Verification responses
+$locale['uf_fb_connect_300'] = "Facebook log ind mailadresse er nu bekræftet";
+$locale['uf_fb_connect_301'] = "Tak for din bekræftelse. Dit Facebook log ind er nu aktiveret.";
+$locale['uf_fb_connect_302'] = "Ugyldig mailadresse.";
+$locale['uf_fb_connect_303'] = "Vi kunne ikke bekræfte mailadressen. Forsøg venligst at forbinde din Facebook log ind igen.";
+$locale['uf_fb_connect_304'] = "Aktiveringskode udløbet.";
+$locale['uf_fb_connect_305'] = "Aktiveringsmailen er udløbet. Venligst forsøg at forbinde din Facebook log ind igen.";
+$locale['uf_fb_connect_306'] = "Ugyldig aktiveringskode.";
+$locale['uf_fb_connect_307'] = "Aktiveringskoden er ugyldig. Forsøg venligst at forbinde din Facebook log ind igen.";
+
+
 // User Fields Connector
 $locale['uf_fb_connect_400'] = 'Facebook er endnu ikke forbundet til din brugerkonto';
 $locale['uf_fb_connect_401'] = 'Facebook er forbundet til din konto';
 $locale['uf_fb_connect_402'] = 'Forbind brugerkonto til Facebook';
 $locale['uf_fb_connect_403'] = 'Deaktiver brugerkonto fra Facebook';
+$locale['uf_fb_connect_404'] = "Forbind din Facebook konto med din {SITE_NAME} brugerkonto.";
+$locale['uf_fb_connect_405'] = "Din Facebook konto kræver aktivering.\nVenligst tjek din registrerede Facebook mail for aktiveringslinket.";
+$locale['uf_fb_connect_406'] = "Tilbage til hovedsiden";
 
 // Email
 $locale['uf_fb_connect_500'] = "{SITE_NAME} - Konto mail aktivering";
 $locale['uf_fb_connect_501'] = "Kære {USER_NAME},\n\nDu har fornyligt forsøgt at linke din Facebook konto med {SITE_NAME} brugerkonto. For at færdiggøre konto synkroniseringen klik da på linket nedenfor:\n{LINK}\n\nVi råder dig til at aktivere din konto indenfor 3 dage efter modtagelsen af denne mail, for at undgå kontoudløb og afslutning af tilknyttede services.\n\nMed venlig hilsen,\n{ADMIN_NAME}, {SITE_NAME}";
 $locale['uf_fb_connect_502'] = "Vi har sent en bekræftelsesmail til din Facebook mailadresse.";
-$locale['uf_fb_connect_503'] = "Denne Facebook konto er blevet associeret med en konto.";
+$locale['uf_fb_connect_503'] = "Denne Facebook konto er blevet associeret med en eksisterende brugerkonto.";
+$locale['uf_fb_connect_504'] = "Du er allerede forbundet til din Facebook";
+$locale['uf_fb_connect_505'] = "Du er nu forbundet til din Facebook konto.";
