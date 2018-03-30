4eda2cbbcbad239cdbce891dec988b0e59101040
RobiNN <RobiNN1@users.noreply.github.com>
2018-03-30 22:44:22 +0200

    Update Czech locale - login infusion

---
+++ Czech/infusions/login/locale/Czech/login.php
@@ -0,0 +1,49 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: infusions/login/locale/Czech/login.php
+| Author: https://www.phpfusion.cz/ - Team
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+/*
+ * Plugins
+ */
+$locale['login_000'] = "Přihlášení";
+$locale['login_001'] = "Přihlašovací systém umožňuje spravovat vaše uživatelské účty s autentizační systémem třetí strany.";
+$locale['login_002'] = "Správa přihlašování";
+
+/*
+ * Administration
+ */
+
+$locale['login_100'] = "Ovladače přihlašování";
+$locale['login_101'] = "Typ";
+$locale['login_102'] = "Verze";
+$locale['login_103'] = "Stav";
+$locale['login_104'] = "Konfigurace";
+$locale['login_105'] = "Nainstalován";
+$locale['login_106'] = "Nenainstalován";
+$locale['login_107'] = "Instalovat ovladač";
+$locale['login_108'] = "Odinstalovat ovladač";
+$locale['login_109'] = "Nakonfigurováno";
+$locale['login_110'] = "Není nakonfigurováno";
+$locale['login_111'] = "Nakonfigurovat {USER_FIELD}";
+
+$locale['login_120'] = "{DRIVER_NAME} byl úspěšně nainstalován";
+$locale['login_121'] = "{DRIVER_NAME} byl odinstalován";
+$locale['login_122'] = "Varování! Odinstalováním ovladače se deaktivují a odstraní informace z účtu uživatele. Opravdu chcete pokračovat?";
+$locale['login_123'] = "Neexistují žádné konfigurace. Ověřte konfigurace nastavení ovladače.";
+$locale['login_124'] = "Zpět na stránku s instalací ovladače";
+$locale['login_125'] = "Nebyl nalezen žádný konfigurační soubor";
+$locale['login_126'] = "Neplatný ovladač/Neznámá chyba";
+$locale['login_127'] = "Konfigurace byly aktualizovány";
---
+++ Czech/infusions/login/locale/Czech/user_fb_connect.php
@@ -0,0 +1,73 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: infusions/login/locale/Czech/user_fb_connect.php
+| Author: https://www.phpfusion.cz/ - Team
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+$locale['uf_fb_connect'] = "Facebook přihlášení";
+$locale['uf_fb_connect_desc'] = "Facebook přihlašovací konektor";
+$locale['uf_fb_connect_verify'] = "Facebook potvrzení přihlášení";
+
+// Driver configuration form
+$locale['uf_fb_connect_200'] = "Facebook přihlášení pomocí Facebook SDK umožňuje lidem přihlásit se na vaši webovou stránku s jejich pověření na Facebooku.";
+$locale['uf_fb_connect_201'] = "Nastavení přihlašování Facebooku";
+$locale['uf_fb_connect_202'] = "Facebook App ID";
+$locale['uf_fb_connect_203'] = "Facebook App Secret";
+$locale['uf_fb_connect_204'] = "Zadejte Vaše Facebook Developer App ID";
+$locale['uf_fb_connect_205'] = "Chcete-li použít tuto funkci přihlášení, musíte mít platný Facebook Client App ID";
+$locale['uf_fb_connect_206'] = "Pokud nemáte App ID, vytvořte a načtěte Facebook Client App ID na své stránce Facebook Developer.";
+$locale['uf_fb_connect_207'] = "Konfigurace doplňku";
+$locale['uf_fb_connect_208'] = "Šířka";
+$locale['uf_fb_connect_209'] = "Šířka (v pixelech) tlačítka doplňku";
+$locale['uf_fb_connect_210'] = "Velikost tlačítka";
+$locale['uf_fb_connect_211'] = "Malé";
+$locale['uf_fb_connect_212'] = "Střední";
+$locale['uf_fb_connect_213'] = "Velké";
+$locale['uf_fb_connect_214'] = "Zobrazit obličeje přátel";
+$locale['uf_fb_connect_215'] = "Zobrazit profilové fotografie přátel, kteří používali tuto stránku";
+$locale['uf_fb_connect_216'] = "Připojit jméno a obrázek profilu, když je uživatel přihlášen na Facebooku";
+$locale['uf_fb_connect_217'] = "Max. řádků fotografií";
+$locale['uf_fb_connect_218'] = "Maximální počet řádků profilových fotografií, které se mají zobrazit";
+$locale['uf_fb_connect_219'] = "Text tlačítka";
+$locale['uf_fb_connect_220'] = "Pokračovat s...";
+$locale['uf_fb_connect_221'] = "Přihlásit sa s...";
+$locale['uf_fb_connect_222'] = "Povolit tlačítko odhlášení";
+$locale['uf_fb_connect_223'] = "Uložit nastavení Facebooku";
+
+// Verification responses
+$locale['uf_fb_connect_300'] = "Přihlašovací e-mailová adresa Facebooku je nyní ověřena";
+$locale['uf_fb_connect_301'] = "Děkujeme za vaše ověření. Vaše přihlašovací jméno je nyní aktivováno.";
+$locale['uf_fb_connect_302'] = "Neplatná e-mailová adresa.";
+$locale['uf_fb_connect_303'] = "E-mailovou adresu jsme nemohli ověřit. Pokuste se znovu připojit svůj Facebook přihlašovací účet.";
+$locale['uf_fb_connect_304'] = "Platnost aktivačního kódu vypršela.";
+$locale['uf_fb_connect_305'] = "Platnost aktivačního e-mailu vypršela. Pokuste se znovu připojit svůj Facebook přihlašovací účet.";
+$locale['uf_fb_connect_306'] = "Neplatný aktivační kód.";
+$locale['uf_fb_connect_307'] = "Aktivační kód je neplatný. Pokuste se znovu připojit svůj Facebook přihlašovací účet.";
+
+// User Fields Connector
+$locale['uf_fb_connect_400'] = "Facebook ještě není připojen k vašemu uživatelskému účtu";
+$locale['uf_fb_connect_401'] = "Facebook je připojen k vašemu účtu";
+$locale['uf_fb_connect_402'] = "Připojit uživatelský účet na Facebooku";
+$locale['uf_fb_connect_403'] = "Odpojit uživatelský účet od Facebooku";
+$locale['uf_fb_connect_404'] = "Propojit účet Facebooku se svým uživatelským účtem {SITE_NAME}.";
+$locale['uf_fb_connect_405'] = "Máte Facebook účet čekající na aktivaci.<br/>Zkontrolujte registrovaný e-mail Facebooku pro aktivační odkaz.";
+$locale['uf_fb_connect_406'] = "Späť na hlavnú stránku";
+
+// Email
+$locale['uf_fb_connect_500'] = "{SITE_NAME} - Aktivace e-mailu účtu";
+$locale['uf_fb_connect_501'] = "Milý/á {USER_NAME},<br/><br/>Nedávno jste se pokusili propojit svůj účet Facebook s účtem na {SITE_NAME}. Chcete-li dokončit synchronizaci účtu, klikněte na níže uvedený odkaz:<br/>{LINK}<br/><br/>Doporučujeme vám aktivovat svůj účet do 3 dnů od přijetí tohoto e-mailu, abyste předešli vypršení účtu a ukončení souvisejících služeb.<br/><br/>Upřímně,<br/>{ADMIN_NAME}, {SITE_NAME}";
+$locale['uf_fb_connect_502'] = "Na vaši e-mailovou adresu na Facebooku jsme odeslali ověřovací e-mail.";
+$locale['uf_fb_connect_503'] = "Tento Facebook účet byl přiřazen k existujícímu uživatelskému účtu.";
+$locale['uf_fb_connect_504'] = "Už jste připojen k vašemu Facebooku";
+$locale['uf_fb_connect_505'] = "Úspěšně jste připojili svůj Facebook účet.";
