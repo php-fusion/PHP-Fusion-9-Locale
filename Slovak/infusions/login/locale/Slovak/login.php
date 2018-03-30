81af444dae15e6e00b1cad26d114498c9e932fe4
RobiNN <RobiNN1@users.noreply.github.com>
2018-03-30 22:44:42 +0200

    Update Slovak locale - login infusion

---
+++ Slovak/infusions/login/locale/Slovak/login.php
@@ -0,0 +1,49 @@
+<?php
+/*-------------------------------------------------------+
+| PHP-Fusion Content Management System
+| Copyright (C) PHP-Fusion Inc
+| https://www.php-fusion.co.uk/
++--------------------------------------------------------+
+| Filename: infusions/login/locale/Slovak/login.php
+| Author: RobiNN
++--------------------------------------------------------+
+| This program is released as free software under the
+| Affero GPL license. You can redistribute it and/or
+| modify it under the terms of this license which you
+| can read by viewing the included agpl.txt or online
+| at www.gnu.org/licenses/agpl.html. Removal of this
+| copyright header is strictly prohibited without
+| written permission from the original author(s).
++--------------------------------------------------------*/
+/*
+ * Plugins
+ */
+$locale['login_000'] = "Prihlásenie";
+$locale['login_001'] = "Prihlasovací systém vám umožňuje spravovať vaše užívateľské účty s autentizačním systémom tretej strany.";
+$locale['login_002'] = "Správa prihlasovania";
+
+/*
+ * Administration
+ */
+
+$locale['login_100'] = "Ovládače prihlasovania";
+$locale['login_101'] = "Typ";
+$locale['login_102'] = "Verzia";
+$locale['login_103'] = "Stav";
+$locale['login_104'] = "Konfigurácia";
+$locale['login_105'] = "Nainštalovaný";
+$locale['login_106'] = "Nenainštalovaný";
+$locale['login_107'] = "Inštalovať ovládač";
+$locale['login_108'] = "Odinštalovať ovládač";
+$locale['login_109'] = "Nakonfigurované";
+$locale['login_110'] = "Nie je nakonfigurované";
+$locale['login_111'] = "Nakonfigurovať {USER_FIELD}";
+
+$locale['login_120'] = "{DRIVER_NAME} bol úspešne nainštalovaný";
+$locale['login_121'] = "{DRIVER_NAME} bol odinštalovaný";
+$locale['login_122'] = "Varovanie! Odinštalovaním ovládača sa deaktivujú a odstránia informácie z účtu užívateľa. Naozaj chcete pokračovať?";
+$locale['login_123'] = "Neexistujú žiadne konfigurácie. Overte konfigurácie nastavení ovládača.";
+$locale['login_124'] = "Späť na stránku s inštaláciou ovládača";
+$locale['login_125'] = "Nenašiel sa žiadny konfiguračný súbor";
+$locale['login_126'] = "Neplatný ovládač/Neznáma chyba";
+$locale['login_127'] = "Konfigurácie boli aktualizované";
