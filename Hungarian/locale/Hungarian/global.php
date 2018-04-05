6cde99697e77553a15062d0e01d4f05c00a87d5b
karrak1 <g.sebestyen@fusionjatek.hu>
2018-04-05 05:51:52 +0200

    git

--- Hungarian/locale/Hungarian/global.php
+++ Hungarian/locale/Hungarian/global.php
@@ -514,6 +514,6 @@ $locale['fmt_points'] = "pont|pontok";
 $locale['fmt_admin'] = "admin|adminok";
 $locale['fmt_group'] = "csoport|csoportok";
 $locale['fmt_category'] = "kategória|kategóriák";
-$locale['fmt_review'] = "felülvizsgálat|felülvizsgálatok";
+$locale['fmt_review'] = "értékelés|értékelés";
 // include Defender locales
 include __DIR__."/defender.php";
