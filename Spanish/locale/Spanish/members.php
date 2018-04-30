873519557d7ab494c1a3efd40adeaa1b598570ee
Talocha <mateofds@hotmail.com>
2018-04-30 12:39:00 +0200

    Update members.php

--- Spanish/locale/Spanish/members.php
+++ Spanish/locale/Spanish/members.php
@@ -1,11 +1,20 @@
 <?php
-$locale['400'] = "Lista de Miembros";
-$locale['401'] = "Nombre de Usuario";
-$locale['402'] = "Tipo de Usuario";
-$locale['403'] = "No hay nombres de usuario que empiecen por ";
-$locale['404'] = "Mostrar Todos";
-$locale['405'] = "Grupos del Usuario";
-$locale['406'] = "Ningún Grupo";
-$locale['407'] = "Todos los Grupos";
-$locale['408'] = "Buscar por miembro:";
-$locale['409'] = "Buscar";
+$locale['MEMB_000'] = "Lista de Miembros";
+$locale['MEMB_001'] = "Avatar";
+$locale['MEMB_002'] = "Nombre de Usuario";
+$locale['MEMB_003'] = "Grupos del Usuario";
+$locale['MEMB_004'] = "Tipo de Usuario";
+$locale['MEMB_005'] = "Buscar miembro ...";
+$locale['MEMB_006'] = "Buscar";
+$locale['MEMB_007'] = "Ordenar&nbsp;por";
+$locale['MEMB_008'] = "Último activo";
+$locale['MEMB_009'] = "Recién registrado";
+$locale['MEMB_010'] = "Alfabético";
+
+$locale['MEMB_012'] = "Ascendente";
+$locale['MEMB_013'] = "Descendente";
+$locale['MEMB_014'] = "Mostrar todo";
+$locale['MEMB_015'] = "No está registrado en ningún grupo";
+$locale['MEMB_016'] = "Acceso a todos los grupos";
+$locale['MEMB_017'] = "Visita {%start_row%} - {%end_row%} de {%max_row%} activo {%member%}";
+$locale['MEMB_018'] = "No hay usuarios que empiecen por: ";
