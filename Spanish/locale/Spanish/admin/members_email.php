e7fbeb5044b360b6a3420e7c9f37e0e1b95133f3
Mateo UT <mateofds@hotmail.com>
2018-05-04 19:16:30 +0200

    Update members_email.php

--- Spanish/locale/Spanish/admin/members_email.php
+++ Spanish/locale/Spanish/admin/members_email.php
@@ -1,39 +1,22 @@
 <?php
-$locale['email_create_subject'] = "Cuenta Creada en ";
+$locale['email_create_subject'] = "Cuenta Creada en [SITENAME]";
 $locale['email_create_message'] = "Hola [USER_NAME],\n
-Tu cuenta en ".fusion_get_settings('sitename')." ha sido creada.\n
-Ahora ya puedes iniciar sesión con los siguientes datos:\n
+Tu cuenta en [SITENAME] ha sido creada.\nAhora ya puedes iniciar sesión con los siguientes datos:\n
 Nombre de Usuario: [USER_NAME]\n
-Contraseña: [PASSWORD]\n\n
-Saludos,\n
-".fusion_get_settings('siteusername');
-$locale['email_activate_subject'] = "Cuenta Activada en ";
-$locale['email_activate_message'] = "Hola [USER_NAME],\n
-Tu cuenta en ".fusion_get_settings('sitename')." ha sido activada.\n
-Ahora ya puedes iniciar sesión usando el nombre de usuario y la contraseña que elegiste.\n\n
-Saludos,\n
-".fusion_get_settings('siteusername');
-$locale['email_deactivate_subject'] = "Reactivación de Cuenta en ".fusion_get_settings('sitename');
-$locale['email_deactivate_message'] = "Hola [USER_NAME],\n
-Han pasado ".fusion_get_settings('deactivation_period')." días desde la última vez que accediste a ".fusion_get_settings('sitename').". Tu usuario ha sido marcado como inactivo, pero todos los datos de tu cuenta y tus contenidos permanecen intactos.\n
-Si deseas reactivar tu cuenta, pulsa el siguiente enlace:\n
-".fusion_get_settings('siteurl')."reactivate.php?user_id=[USER_ID]&code=[CODE]\n\n
-Saludos,\n
-".fusion_get_settings('siteusername');
-$locale['email_ban_subject'] = "Expulsión de Cuenta en ".fusion_get_settings('sitename');
-$locale['email_ban_message'] = "Hola [USER_NAME],\n
-Tu cuenta en ".fusion_get_settings('sitename')." ha sido expulsada por ".$userdata['user_name'].". El motivo es el siguiente:\n
-[REASON].\n
-Si deseas más información sobre esta expulsión, contacta con el administrador del sitio en ".fusion_get_settings('siteemail').".\n Saludos,\n
-".fusion_get_settings('siteusername');
-$locale['email_secban_subject'] = "Expulsión de Cuenta en ".fusion_get_settings('sitename');
-$locale['email_secban_message'] = "Hola [USER_NAME],\n
-Tu cuenta en ".fusion_get_settings('sitename')." ha sido expulsada por ".$userdata['user_name']." debido a que algunas acciones atribuidas a tí o relacionadas con tu cuenta han sido consideradas como una amenaza de seguridad para el sitio.\n
-Si deseas más información sobre esta expulsión de seguridad, contacta con el administrador del sitio en ".fusion_get_settings('siteemail').".\n Saludos,\n
-".fusion_get_settings('siteusername');
-$locale['email_suspend_subject'] = "Suspensión de Cuenta en ".fusion_get_settings('sitename');
+Contraseña: [PASSWORD]\n\nRegards,\n[SITEUSERNAME]";
+$locale['email_activate_subject'] = "Cuenta Activada en [SITENAME]";
+$locale['email_activate_message'] = "Hola [USER_NAME],\nTu cuenta en [SITENAME] ha sido activada.\n
+Ahora ya puedes iniciar sesión usando el nombre de usuario y la contraseña que elegiste.\n\nSaludos,\n[SITEUSERNAME]";
+$locale['email_deactivate_subject'] = "Reactivación de Cuenta en[SITENAME]";
+$locale['email_deactivate_message'] = "Hola [USER_NAME],\nHan pasado [DEACTIVATION_PERIOD] días desde la última vez que accediste a [SITENAME]. Tu usuario ha sido marcado como inactivo, pero todos los datos de tu cuenta y tus contenidos permanecen intactos.\n\n
+Si deseas reactivar tu cuenta, pulsa el siguiente enlace: [REACTIVATION_LINK]\n\nSaludos,\n[SITEUSERNAME]";
+$locale['email_ban_subject'] = "Su cuenta en [SITENAME] ha sido prohibida";
+$locale['email_ban_message'] = "Hola [USER_NAME],\nTu cuenta en [SITENAME] ha sido expulsada por [ADMIN_USERNAME] El motivo es el siguiente:\n
+[REASON].\nSi deseas más información sobre esta expulsión, contacta con el administrador del sitio en [SITENAME].\n Saludos,\n[SITEUSERNAME]";
+$locale['email_secban_subject'] = "Su cuenta en [SITENAME] ha sido prohibida";
+$locale['email_secban_message'] = "Hola [USER_NAME],\nTu cuenta en [SITENAME] ha sido expulsada por [ADMIN_USERNAME] debido a que algunas acciones atribuidas a tí o relacionadas con tu cuenta han sido consideradas como una amenaza de seguridad para el sitio.\n
+Si deseas más información sobre esta expulsión de seguridad, contacta con el administrador del sitio en [SITENAME].\n Saludos,\n[SITEUSERNAME]";
+$locale['email_suspend_subject'] = "Suspensión de Cuenta en[SITENAME]";
 $locale['email_suspend_message'] = "Hola [USER_NAME],\n
-Tu cuenta en ".fusion_get_settings('sitename')." ha sido suspendida por ".$userdata['user_name']." hasta [DATE] (fecha/hora del sitio) por lo siguiente:\n
-[REASON].\n
-Si deseas más información sobre esta suspensión, contacta con el administrador del sitio en ".fusion_get_settings('siteemail').".\n Saludos,\n
-".fusion_get_settings('siteusername');
+Tu cuenta en [SITENAME] ha sido suspendida por [ADMIN_USERNAME] hasta [DATE] (fecha/hora del sitio) por lo siguiente:\n
+[REASON].\nSi deseas más información sobre esta suspensión, contacta con el administrador del sitio en [SITENAME].\n Saludos,\n[SITEUSERNAME]";
