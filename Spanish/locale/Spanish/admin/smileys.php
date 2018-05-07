3569e9ce9e452eb6cdef295447b0ddd89b06ce4f
Mateo UT <mateofds@hotmail.com>
2018-05-07 19:39:46 +0200

    Update smileys.php

--- Spanish/locale/Spanish/admin/smileys.php
+++ Spanish/locale/Spanish/admin/smileys.php
@@ -1,32 +1,38 @@
 <?php
-// Smiley titles
-$locale['400'] = "Smileys Existentes";
-$locale['401'] = "Añadir Smiley";
-$locale['402'] = "Editar Smiley";
-$locale['403'] = "Smileys";
-// Smiley messages
-$locale['410'] = "Smiley añadido";
-$locale['411'] = "Smiley actualizado";
-$locale['412'] = "Smiley borrado";
-$locale['413'] = "No se ha podido actualizar el Smiley";
-$locale['414'] = "No se ha podido añadir el Smiley";
-$locale['415'] = "El código del Smiley ya está en uso.";
-$locale['416'] = "¿Borrar este Smiley?";
-// Smiley form
-$locale['420'] = "Código:";
-$locale['421'] = "Imagen:";
-$locale['422'] = "Texto:";
-$locale['423'] = "Guardar'";
-$locale['424'] = "Vista Previa:";
-// Current Smileys
-$locale['430'] = "Código";
-$locale['431'] = "Imagen";
-$locale['432'] = "Texto";
-$locale['433'] = "Opciones";
-$locale['434'] = "Editar";
-$locale['435'] = "Borrar";
-$locale['436'] = "No se han creado Smileys.";
-// Error Message
-$locale['437'] = "Introduce un código de Smiley";
-$locale['438'] = "seleccione un Smiley";
-$locale['439'] = "Introduce un texto de Smiley";
+// títulos sonrientes
+$locale['SMLY_400'] = "Smiley actuales";
+$locale['SMLY_401'] = "Agregar Smiley";
+$locale['SMLY_402'] = "Editar Smiley";
+$locale['SMLY_403'] = "Smileys";
+$locale['SMLY_404'] = "Smileys activos";
+$locale['SMLY_405'] = "Smileys inactivos";
+// Mensajes sonrientes
+$locale['SMLY_410'] = "Smiley agregado";
+$locale['SMLY_411'] = "Smiley actualizado";
+$locale['SMLY_412'] = "Smiley eliminado";
+$locale['SMLY_413'] = "Smileys inactivos";
+
+$locale['SMLY_414'] = "El texto sonriente está en uso.";
+$locale['SMLY_415'] = "El código de emoticono está en uso.";
+$locale['SMLY_416'] = "¿Realmente deseas eliminar esta carita sonriente?";
+$locale['SMLY_417'] = "¿Realmente deseas Inactiv esta carita sonriente?";
+$locale['SMLY_418'] = "Error al cargar smiley";
+// forma sonriente
+$locale['SMLY_420'] = "Código Smiley:";
+$locale['SMLY_421'] = "Imagen sonriente:";
+$locale['SMLY_422'] = "Texto sonriente:";
+$locale['SMLY_423'] = "Guardar Smiley";
+$locale['SMLY_424'] = "Actualizar Smiley";
+// Smiley actuales
+$locale['SMLY_430'] = "Código";
+$locale['SMLY_431'] = "Imagen";
+$locale['SMLY_432'] = "Texto";
+$locale['SMLY_433'] = "Opciones";
+$locale['SMLY_435'] = "Inactivo";
+$locale['SMLY_436'] = "No hay emoticones definidos.";
+// Mensaje de error
+$locale['SMLY_437'] = "Por favor ingrese un Código Smiley";
+$locale['SMLY_438'] = "Seleccione un Smiley";
+$locale['SMLY_439'] = "Por favor ingrese un texto sonriente";
+$locale['SMLY_440'] = "Sin Smileys activos";
+$locale['SMLY_441'] = "Sin Smileys inactivos";
