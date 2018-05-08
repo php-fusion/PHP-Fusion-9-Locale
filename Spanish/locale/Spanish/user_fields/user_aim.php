2f9287aef0abcf9fec439253daaa9566396b045f
Mateo UT <mateofds@hotmail.com>
2018-05-08 17:54:15 +0200

    Update user_aim.php

--- Spanish/locale/Spanish/user_fields/user_aim.php
+++ Spanish/locale/Spanish/user_fields/user_aim.php
@@ -2,3 +2,4 @@
 $locale['uf_aim'] = "AIM";
 $locale['uf_aim_desc'] = "Mensajería instantánea de AOL";
 $locale['uf_aim_error'] = "El ID de AIM no puede dejarse vacío.";
+$locale['uf_aim_error_1'] = "ID DE AIM es incorrecto. Utilice una ID de AIM válida";
