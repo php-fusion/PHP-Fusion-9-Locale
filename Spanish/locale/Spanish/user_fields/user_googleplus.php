7f558afa52f62bc3b72d1de25163d0a0434ccf41
Mateo UT <mateofds@hotmail.com>
2018-05-08 20:12:48 +0200

    Create user_googleplus.php

---
+++ Spanish/locale/Spanish/user_fields/user_googleplus.php
@@ -0,0 +1,5 @@
+<?php
+$locale['uf_googleplus'] = "Google+";
+$locale['uf_googleplus_id'] = "URL del perfil de Google+ o nombre de usuario";
+$locale['uf_googleplus_desc'] = "Página de perfil de Google+";
+$locale['uf_googleplus_error'] = "La URL del perfil de Google+ o el nombre de usuario no se pueden dejar vacíos.";
