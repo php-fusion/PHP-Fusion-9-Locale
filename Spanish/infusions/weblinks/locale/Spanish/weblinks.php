85616b4396bbcb78f051825a78e62f6c053eac27
Mateo UT <mateofds@hotmail.com>
2018-05-12 19:44:44 +0200

    Update weblinks.php

--- Spanish/infusions/weblinks/locale/Spanish/weblinks.php
+++ Spanish/infusions/weblinks/locale/Spanish/weblinks.php
@@ -1,9 +1,22 @@
 <?php
-$locale['400'] = "Enlaces Web";
-// Download File Information
-$locale['410'] = "Nuevo";
-$locale['411'] = "Añadido el:";
-$locale['412'] = "Vistas:";
-// Downloads Notices
-$locale['430'] = "No se han creado categorías de enlaces web";
-$locale['431'] = "No hay enlaces web en esta categoría";
+// Overview
+$locale['web_0000'] = "Enlaces web";
+$locale['web_0001'] = "Todos los enlaces web";
+$locale['web_0002'] = "Mostrar categorías";
+$locale['web_0003'] = "Categorías";
+$locale['web_0004'] = "Última actualización";
+$locale['web_0005'] = "Categoría:";
+
+$locale['fmt_open'] = "abrir|abrir";
+// Clasificaciones
+$locale['web_0030'] = "Más reciente";
+$locale['web_0031'] = "Arriba abierto";
+
+// Puntos de vista
+$locale['web_0040'] = "Vista galería";
+$locale['web_0041'] = "Vista lista";
+
+// Mensajes
+$locale['web_0060'] = "No se definieron categorías de enlace web.";
+$locale['web_0061'] = "Sin enlace web definido.";
+$locale['web_0062'] = "No hay un enlace web en esta categoría.";
