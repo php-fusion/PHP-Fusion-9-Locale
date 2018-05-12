c71b4dc39fa15952a59d18126b09c16f0d0f9900
Mateo UT <mateofds@hotmail.com>
2018-05-12 20:29:32 +0200

    Update Spanish.php

--- Spanish/themes/admin_themes/Artemis/locale/Spanish.php
+++ Spanish/themes/admin_themes/Artemis/locale/Spanish.php
@@ -5,4 +5,4 @@ $locale['copyright'] = "Copyright &copy;";
 $locale['render_engine'] = "Rendimiento Motor";
 $locale['artemis_admin'] = "Artemis PHP-Fusion 9";
 $locale['admin_apps'] = "Aplicaciones";
-$locale['admin_collapse'] = "Contraer menú";
+$locale['admin_collapse'] = "Contrae menú";
