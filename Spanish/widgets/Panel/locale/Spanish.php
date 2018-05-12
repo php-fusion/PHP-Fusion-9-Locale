fae1c963b873d6843673ce12e55ab63392da27eb
Mateo UT <mateofds@hotmail.com>
2018-05-12 21:48:54 +0200

    Update Spanish.php

--- Spanish/widgets/Panel/locale/Spanish.php
+++ Spanish/widgets/Panel/locale/Spanish.php
@@ -18,7 +18,7 @@
 $locale['PW_0100'] = "Panel";
 $locale['PW_0101'] = "Paneles PHP-Fusion";
 $locale['PW_0200'] = "Seleccionar panel instalado";
-$locale['PW_0201'] = "Verifique la Configuración de Acceso al Panel instalada en [LINK] Administración del Panel [/ LINK]";
+$locale['PW_0201'] = "Verifique la Configuración de Acceso al Panel instalada en [LINK]Administración del Panel[/LINK]";
 $locale['PW_0220'] = "Guardar panel";
 $locale['PW_0221'] = "Guardar y cerrar panel";
 $locale['PW_0222'] = "Nombre de archivo del panel";
