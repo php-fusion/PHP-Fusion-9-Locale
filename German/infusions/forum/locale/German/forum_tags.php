7405ec9945aed358aa65efa857d456d1cfe7758e
dtechniker <40355496+dtechniker@users.noreply.github.com>
2018-06-26 09:31:38 +0200

    Edit computing case

--- German/infusions/forum/locale/German/forum_tags.php
+++ German/infusions/forum/locale/German/forum_tags.php
@@ -1,24 +1,25 @@
 <?php
 
-$locale['forum_tag_0100'] = "Forum Tags";
-$locale['forum_tag_0101'] = "Tags werden verwendet, um Ihre Threads zu organisieren und Sie bei der Mikroorganisation Ihrer Diskussionsthemen zu unterst&uuml;tzen.";
-$locale['forum_tag_0102'] = "Derzeitiger Forum Tag";
-$locale['forum_tag_0103'] = "Erstelle Tag";
-$locale['forum_tag_0104'] = "Ver&auml;ndere Tag";
-$locale['forum_tag_0105'] = "Tag erstellt";
-$locale['forum_tag_0106'] = "Tag ver&auml;ndert";
-$locale['forum_tag_0107'] = "Tag gel&uuml;scht";
+$locale['forum_tag_0100'] = "Forum Stichworte";
+$locale['forum_tag_0101'] = "Stichworte werden verwendet, um Ihre Threads zu organisieren und Sie bei der Mikroorganisation Ihrer Diskussionsthemen zu unterst&uuml;tzen.";
+$locale['forum_tag_0102'] = "Derzeitiger Forum Stichwort";
+$locale['forum_tag_0103'] = "Erstelle Stichwort";
+$locale['forum_tag_0104'] = "Ver&auml;ndere Stichwort";
+$locale['forum_tag_0105'] = "Stichwort erstellt";
+$locale['forum_tag_0106'] = "Stichwort ver&auml;ndert";
+$locale['forum_tag_0107'] = "Stichwort gel&uuml;scht";
 
 $locale['forum_tag_0110'] = "Seiten Information";
 $locale['forum_tag_0111'] = "Diskussionen &uuml;ber die Verwaltung dieser Website";
 
-$locale['forum_tag_0200'] = "Tag Titel";
-$locale['forum_tag_0201'] = "Tag Beschreibung";
-$locale['forum_tag_0202'] = "Tag Farbe";
-$locale['forum_tag_0203'] = "Tag Sprache";
-$locale['forum_tag_0204'] = "Tag Status";
-$locale['forum_tag_0205'] = "Dieser tag ist active";
-$locale['forum_tag_0206'] = "Dieser tag ist inactive";
-$locale['forum_tag_0207'] = "Erstelle Tag";
-$locale['forum_tag_0208'] = "Ver&auml;ndere Tag";
+$locale['forum_tag_0200'] = "Stichwort Titel";
+$locale['forum_tag_0201'] = "Stichwort Beschreibung";
+$locale['forum_tag_0202'] = "Stichwort Farbe";
+$locale['forum_tag_0203'] = "Stichwort Sprache";
+$locale['forum_tag_0204'] = "Stichwort Status";
+$locale['forum_tag_0205'] = "Dieser Stichwort ist active";
+$locale['forum_tag_0206'] = "Dieser Stichwort ist inactive";
+$locale['forum_tag_0207'] = "Erstelle Stichwort";
+$locale['forum_tag_0208'] = "Ver&auml;ndere Stichwort";
+
 ?>
