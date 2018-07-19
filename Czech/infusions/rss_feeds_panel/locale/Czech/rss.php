c86c63ff104ce3e79eb682d87ca095ccc3856ee0
robinn1 <robokelcak@centrum.sk>
2018-07-20 00:31:31 +0200

    Fix locale

--- Czech/infusions/rss_feeds_panel/locale/Czech/rss.php
+++ Czech/infusions/rss_feeds_panel/locale/Czech/rss.php
@@ -22,5 +22,5 @@ $locale['rss_downloads'] = "Soubory ke stažení";
 $locale['rss_forums'] = "Fórum";
 $locale['rss_news'] = "Novinky";
 $locale['rss_weblinks'] = "Webové odkazy";
-$locale['rss_in'] = " Kategorie ";
+$locale['rss_in'] = " v ";
 $locale['rss_nodata'] = "Žádné RSS údaje nejsou k dispozici";
