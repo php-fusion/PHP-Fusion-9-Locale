3a85c88d0b2b3ad27af91970d685a575e915d85b
karrak1 <g.sebestyen@fusionjatek.hu>
2018-08-15 13:28:23 +0200

    delete unnecessary locale file

--- Hungarian/locale/Hungarian/bbcodes/flash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$locale['bb_flash_description'] = "Flash videó megjelenítése URL alapján";
-$locale['bb_flash_usage'] = "Flash URL";
-$locale['bb_flash_width'] = "Szélesség (px)";
-$locale['bb_flash_height'] = "Magasság (px)";
